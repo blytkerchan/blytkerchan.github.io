89eea6bf47b9e72c5c2c629ba13e4c1ed0045b02
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2022-12-16 18:25:03 -0500

    allow markdown on pages

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,13 +1,18 @@
 import { useTranslation } from "react-i18next";
+import ReactMarkdown from 'react-markdown';
+import { Link } from 'react-router-dom';
 
 const Page = (props) => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <h2>{t(`pages:${props.name}.title`)}</h2>
-      <p>{t(`pages:${props.name}.content`)}</p>
-    </>
+    <ReactMarkdown components={
+      {
+        a: (props) => <Link to={props.href}>{props.children}</Link>
+      }
+    }>
+      {t(`pages:${props.name}.content`)}
+    </ReactMarkdown>
   );
 };
 
