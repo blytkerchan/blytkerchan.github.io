87720b716396ab132c0ffb8613743caf02842372
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2022-12-27 13:23:04 -0500

    fix case sensitivity using the collation

--- test-server/src/app/schema/Credentials.js
+++ test-server/src/app/schema/Credentials.js
@@ -5,21 +5,18 @@ function Credentials({ env }) {
     {
       username: {
         type: String,
-        unique: true,
-        collation: { locale: `${env.locale}`, strength: 2 },
-        required: true
+        index: {
+          unique: true,
+          collation: { locale: `${env.locale}`, strength: 2 },
+        },
+        required: true,
       },
       hash: { type: String, required: true },
     },
     { timestamps: true, strict: true, strictQuery: true }
   );
-  retval.pre('save', async function() {
-    this.username = this.username.toLowerCase();
-  });
 
   return retval;
 }
-
-
 
 module.exports = Credentials;
