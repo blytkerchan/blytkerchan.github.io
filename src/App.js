4360d2484bea6572bddf1eb9d44f522f06f8b9b8
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2022-12-27 01:10:34 -0500

    re-include javascript for bootstrap: still needed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 
