978f01931fc12d9a14f01b6c3306604e7f66a809
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2022-12-27 03:09:41 -0500

    robustify saveToken

--- src/useToken.js
+++ src/useToken.js
@@ -1,23 +1,30 @@
 import { useState } from "react";
 
 export default function useToken() {
-    const getToken = () => {
-      const tokenString = sessionStorage.getItem("token");
-      const userToken = JSON.parse(tokenString);
-      return userToken?.token;
-    };
-  
-    const [token, setToken] = useState(getToken());
-  
-    const saveToken = (userToken) => {
-      sessionStorage.setItem("token", JSON.stringify(userToken));
-      setToken(userToken.token);
-    };
-  
-    return {
-      setToken: saveToken,
-      token,
-    };
-  }
-  
-  
+  const getToken = () => {
+    let tokenString = sessionStorage.getItem("token");
+    if (tokenString === null) {
+      tokenString = localStorage.getItem("token");
+    }
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token;
+  };
+
+  const [token, setToken] = useState(getToken());
+
+  const saveToken = ({ token, remember }) => {
+    if (typeof token !== "undefined" && token !== null) {
+      if (remember) {
+        localStorage.setItem("token", JSON.stringify(token));
+      } else {
+        sessionStorage.setItem("token", JSON.stringify(token));
+      }
+      setToken(token.token);
+    }
+  };
+
+  return {
+    setToken: saveToken,
+    token,
+  };
+}
