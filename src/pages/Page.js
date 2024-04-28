d268ef7c443eea8fce9018dd37e3575db645e6a7
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2024-04-28 01:07:23 -0400

    Implement the about page, and the generic page logic

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,18 +1,27 @@
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import ReactMarkdown from "react-markdown";
+import Markdown from "react-markdown";
 import { Link } from "react-router-dom";
 
 const Page = (props) => {
   const { t } = useTranslation();
 
+  const [contents, setContents] = useState("");
+
+  useEffect(() => {
+    fetch(`/${props.name}.md`)
+      .then((res) => res.text())
+      .then((text) => setContents(text));
+  });
+
   return (
-    <ReactMarkdown
+    <Markdown
       components={{
         a: (props) => <Link to={props.href}>{props.children}</Link>,
       }}
     >
-      {t(`pages:${props.name}.content`)}
-    </ReactMarkdown>
+      {contents}
+    </Markdown>
   );
 };
 
