37bc69cb622441e965412941b1619d613de3b459
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2024-05-29 19:19:09 -0400

    Implement Opengraph metadata (#48)
    This adds a Lambda@Edge that inserts the metadata into the index.html
    which will be downloaded by the front-end/post inspector
    * fix compile-time error introduced by newer version of dependency
    * generate meta files (#47)
    * Don't automatically deploy on build
    * Make the build script executable and minimize build
    * Issue #47: set all metadata, including thumbnail
    * Bump da version

--- src/index.js
+++ src/index.js
@@ -4,11 +4,39 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import environment from "./config/environment";
 
+import useMeta from "./lib/useMeta";
 import App from "./App";
 
 import "./i18n";
 
 import reportWebVitals from "./lib/reportWebVitals";
+
+const { fetchMeta } = useMeta();
+fetchMeta()
+  .then((res) => {
+    console.log(res);
+    return res.json();
+  })
+  .then((meta) => {
+    const headElement = document.getElementById("head");
+    Object.keys(meta).forEach((key) => {
+      if (key === "title") {
+        const titleNode = document.createElement("title");
+        const title = document.createTextNode(meta[key]);
+        titleNode.appendChild(title);
+        headElement.appendChild(titleNode);
+      } else {
+        const metaNode = document.createElement("meta");
+        const nameAttribute = document.createAttribute("name");
+        nameAttribute.value = key;
+        const contentAttribute = document.createAttribute("content");
+        contentAttribute.value = meta[key];
+        metaNode.setAttributeNode(nameAttribute);
+        metaNode.setAttributeNode(contentAttribute);
+        headElement.appendChild(metaNode);
+      }
+    });
+  });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
